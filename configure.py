7036adee875734edda706a176a9d5965014616cc
Justus Schock <12886177+justusschock@users.noreply.github.com>
2021-07-22 09:22:44 +0000

    apply changes to setup.cfg

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ def setup_cfg_mods(line: str) -> str:
     line = line.replace("REPOSITORY", args.repository)
     if os.path.isfile('LICENSE.rst'):
         line = line.replace("LICENSE", "LICENSE.rst")
-    elif not os.path.isfile('LICENSE'):
+    elif not os.path.isfile('LICENSE') and 'LICENSE' in line:
         line = ''
 
     return line
@@ -19,6 +19,11 @@ parser.add_argument('--repository', type=str)
 args = parser.parse_args()
 if args.package_name is None:
     args.package_name = args.repository.split('/', 1)[-1]
+
+with open('setup.cfg', 'r') as f:
+    content = [setup_cfg_mods(l) for l in f.readlines()]
+with open('setup.cfg', 'w') as f:
+    f.writelines(content)
 
 shutil.move("PACKAGE", args.package_name)
 
